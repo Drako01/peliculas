62e8ae2eb90154c89f3875638dd06c63627e8d8b
Alejandro Daniel Di Stefano <88512335+Drako01@users.noreply.github.com>
2022-05-10 19:03:58 -0300

    10052022

--- app_peliculas/titulos/forms.py
+++ app_peliculas/titulos/forms.py
@@ -3,10 +3,10 @@ from django import forms
 class FormularioPeliculas(forms.Form):    
     nombre = forms.CharField(label="Nombre", max_length=128)
     GENERO = (
-        (1, "Accion"),
-        (2, "Drama"),
-        (3, "Romantica"),
-        (4, "Infantil")
+        ("ACC", "Accion"),
+        ("DRA", "Drama"),
+        ("ROM", "Romantica"),
+        ("INF", "Infantil")
     )
     genero = forms.ChoiceField(label="Género", choices=GENERO)
     fecha = forms.DateField(
@@ -15,9 +15,9 @@ class FormularioPeliculas(forms.Form):
     )
     cantidad = forms.IntegerField(label="Cantidad de Ejemplares: ")
     EDADES = (
-        (1, "ATP - Apto para Todo Público"),
-        (2, "Peliculas Infantiles"),
-        (3, "+18 - Contenido para Adultos")
+        ("ATP", "ATP - Apto para Todo Público"),
+        ("INF", "INF - Peliculas Infantiles"),
+        ("+18", "+18 - Contenido para Adultos")
     )
     edades = forms.ChoiceField(label="Audiencia: ", choices=EDADES)
     preventa = forms.BooleanField(label="¿Preventa OnLine?", required=False)
